0c9904e8dee4802f6f361177393d1f8944ba9554
qinyuncao <qinyuncao@umass.edu>
2023-04-28 15:19:52 -0400

    Update

--- src/Order_Service/order.py
+++ src/Order_Service/order.py
@@ -12,8 +12,7 @@ lock = Lock()
 # count1 = -1
 # count2 = -1
 # count3 = -1
-#----------会不会更简洁----------改了58-59行，109-119行#
-ports = [6000,6001,6002]
+ports = [6000, 6001, 6002]
 counts = [-1, -1, -1]
 
 
@@ -55,23 +54,17 @@ def trade(c, msg, leader_port):
         reply_msg = '{code}/{payload}'.format(code=200, payload=payload)
         c.send(reply_msg.encode())
 
-        cur_counter = -1
         i = ports.index(leader_port)
-        cur_counter = count[i] + 1
-        # ----------原代码----------#
-        # if port == 6000:
-        #     curr_counter = count1+1
-        # elif port == 6001:
-        #     curr_counter = count2+1
-        # elif port == 6002:
-        #     curr_counter = count3+1
+        cur_counter = counts[i] + 1
 
-        order_history = cur_counter + ' stockName:' + stockName + ",tradeType:" + tradeType + ",quantity:" + str(quantity) + '\n'
-        file = "order"+str(leader_port)+".txt"
+        order_history = str(cur_counter) + ' stockName:' + stockName + ",tradeType:" + tradeType + ",quantity:" + str(
+            quantity) + '\n'
+        file = "order" + str(leader_port) + ".txt"
         with open(file, 'w+') as file:
             # write to leader's own database file
             file.write(order_history)
 
+        #通知follower
         notify_msg = 'notify {order_his}'.format(order_his=order_history)
         for port in ports:
             if port != leader_port:
@@ -83,14 +76,14 @@ def trade(c, msg, leader_port):
                 follower_socket.close()
 
 
-#可能有order, leader通知(从frontend），leader给follower发消息，healthcheck（从frontend和从order到order),crash掉以后的同步申请
+# 可能有order, leader通知(从frontend），leader给follower发消息，healthcheck（从frontend和从order到order),crash掉以后的同步申请
 def handle_client(client_socket, port):
     request = client_socket.recv(1024).decode('utf-8')
     action = request.splitlines()[0].split(' ')[0]
     if action == 'order':
         # leader handle order requests from front end
-        trade(client_socket, request,port)
-    elif action == 'health check':
+        trade(client_socket, request, port)
+    elif action == 'healthCheck':
         # all order services handle 'health check' from frontend
         health_msg = 'alive {self_port}'.format(self_port=port)
         client_socket.send(health_msg.encode())
@@ -107,6 +100,7 @@ def handle_client(client_socket, port):
             file.write(order_history)
     return
 
+
 def start_server(port):
     # create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -118,12 +112,12 @@ def start_server(port):
     # accept incoming connections and handle them in separate threads
     while True:
         client_socket, address = server_socket.accept()
-        client_thread = Thread(target=handle_client, args=(client_socket,port))
+        client_thread = Thread(target=handle_client, args=(client_socket, port))
         client_thread.start()
 
 
 if __name__ == '__main__':
-    #port = 9090 改frontend
+    # port = 9090 改frontend
 
     # start multiple servers in separate threads
     server_threads = []
@@ -135,38 +129,9 @@ if __name__ == '__main__':
                 lines = file.readlines()
                 for line in reversed(lines):
                     if int(line.split(' ')[0]) > 0:
-                        count[i] = int(line.split(' ')[0])
+                        counts[i] = int(line.split(' ')[0])
         else:
-            count[i] = 0
-    #--------------------原代码--------------------#
-    # for port in range(6000, 6003):
-    #     filename = "order" + str(port) + ".txt"
-    #     if os.path.exists(filename):
-    #         if port == 6000:
-    #             with open(filename, 'r') as file:
-    #                 lines = file.readlines()
-    #                 for line in reversed(lines):
-    #                     if int(line.split(' ')[0]) > 0:
-    #                         count1 = int(line.split(' ')[0])
-    #         elif port == 6001:
-    #             with open(filename, 'r') as file:
-    #                 lines = file.readlines()
-    #                 for line in reversed(lines):
-    #                     if int(line.split(' ')[0]) > 0:
-    #                         count2 = int(line.split(' ')[0])
-    #         elif port == 6002:
-    #             with open(filename, 'r') as file:
-    #                 lines = file.readlines()
-    #                 for line in reversed(lines):
-    #                     if int(line.split(' ')[0]) > 0:
-    #                         count3 = int(line.split(' ')[0])
-    #     else:
-    #         if port == 6000:
-    #             count1 = 0
-    #         elif port == 6001:
-    #             count2 = 0
-    #         elif port == 6002:
-    #             count2 = 0
+            counts[i] = 0
 
         server_thread = Thread(target=start_server, args=(port,))
         server_thread.start()
